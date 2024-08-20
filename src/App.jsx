27cbffd4bcecbe5718848760aca8b4ed625547ef
Kim Delgado <kimdelgado1570@gmail.com>
2024-08-20 16:55:22 +0200

    create releases page components and routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import SignupPage from "./Components/SignupPage"
 import LoginPage from './Components/LoginPage'
 import { useContext } from "react";
 import { AuthContext } from "./context/user.context";
-
+import ReleasesPage from './Pages/ReleasesPage';
 
 
 
@@ -21,6 +21,10 @@ function App() {
         <Route path="/" element={<h1>This is home</h1>} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/releases" element={<ReleasesPage />}/>
+        <Route path="/releases/:releaseId" element={<h1>Release Details</h1>}/>
+        <Route path="/products" element={<ProductsPage />}/>
+        <Route path="/products/:productId" element={<h1>Product Details</h1>}/>
       </Routes>
     </>
   )
---
+++ src/Pages/ReleasesPage.jsx
@@ -0,0 +1,52 @@
+import ReleaseCard from "../Components/ReleaseCard"
+import CreateReleaseForm from "../Components/CreateReleaseForm"
+import { useState, useEffect } from "react"
+import axios from "axios"
+import './ReleasesPage.css'
+
+const API_URL = import.meta.env.VITE_API_URL
+
+
+function ReleasesPage() {
+    const [displayForm, setDisplayForm] = useState(false)
+    const [releases, setReleases] = useState([])
+    
+    const handleDisplayForm = () => {
+        return setDisplayForm(!displayForm)
+    }
+
+    function getReleases() {
+        axios.get(`${API_URL}/api/releases`)
+            .then((response) => {
+                setReleases(response.data);
+            })
+            .catch((error) => {
+                console.error(error.response.data.message);
+            });
+    }
+
+    useEffect(() =>{
+        getReleases()
+    }, [])
+
+    return(
+        <section  id="releases-page">
+            <div id="release-form-container">
+                <button onClick={handleDisplayForm}>Make Release</button>
+                { displayForm && <CreateReleaseForm onClick={handleDisplayForm}/>}
+                
+
+            </div>
+            <ul id="card-container">
+                {
+                    releases && 
+                    releases.map(({_id, title, producer, imageUrl}) => {
+                        return <ReleaseCard key={_id} title={title} producer={producer} imageUrl={imageUrl}/>
+                    })
+                }
+            </ul>
+        </section>
+    )
+}
+
+export default ReleasesPage
