ce8e9546fd3ddb5cb9c40823808f6fcabb45de6a
Kim Delgado <kimdelgado1570@gmail.com>
2024-08-21 11:35:01 +0200

    finish CreateRelease form logic

--- src/Components/EditReleaseForm.jsx
+++ src/Components/EditReleaseForm.jsx
@@ -11,13 +11,39 @@ function EditReleaseForm({title, producer, imageUrl, _id, date}) {
     const [newImageUrl, setNewImageUrl] = useState(imageUrl)
     const [errorMessage, setErrorMessage] = useState()
 
-    const handleEditRelease = () => {
+    const authToken = localStorage.getItem("Authorization")
 
+    const handleEditRelease = (e) => {
+        e.preventDefault()
+
+        const updatedRelease = {
+            newTitle,
+            newDate,
+            newProducer,
+            newImageUrl,
+        }
+
+        if(authToken) {
+            axios.put(`${API_URL}/api/releases`, updatedRelease, { headers: { Authorization: `Bearer ${authToken}`} })
+            .then((response) => {
+                console.log("Release updated!", response);
+                location.reload()
+            })
+            .catch((error) => {
+                console.error(error);
+                setErrorMessage(error.response.data.message);
+        
+                setTimeout(() => {
+                    setErrorMessage(null);
+                }, 10000);
+            });
+        }
+        
     }
 
     return(
         <section className="form-section">
-            <form>
+            <form onSubmit={handleEditRelease}>
                 <label htmlFor="title">Title</label>
                 <label htmlFor="title">Title</label>
                 <input onChange={(e) => setNewTitle(e.target.value)} type="text" name="title" id="title" value={newTitle} />
