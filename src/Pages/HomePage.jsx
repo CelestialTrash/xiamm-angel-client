41d0ac501a559a259c357127b93fb3488393b466
Aquiles <Aquileshr92@gmail.com>
2024-08-22 11:45:30 +0200

    css init

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,7 +3,7 @@ import './HomePage.css'
 
 function HomePage() {
     return(
-        <>
+        <section className='home-page-section'>
         <div className="social-links-section">
             <div className='social-links-container'>
             <p>S</p>
@@ -16,13 +16,20 @@ function HomePage() {
 
         </div>
         <div className="image-container">
-
+            {/* <h2>test</h2>
+            <h3>test</h3>
+            <h4>test</h4>
+            <h5>test</h5>
+            <h6>test</h6>
+            <p> mmodi officiis exercitationem itaque aliquam illo recusandae, 
+            non explicabo quia consequuntur quod?</p> */}
+            
         </div>
         <div className='title-container'>
         <h1>Xyamm Angel</h1>
         </div>
         
-        </>
+        </section>
     )
     
 }
