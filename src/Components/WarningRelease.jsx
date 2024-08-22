396c53f1795ef931378de36e69b21f75bb5d1b0c
Kim Delgado <kimdelgado1570@gmail.com>
2024-08-22 17:30:53 +0200

    style warnings

--- src/Components/WarningRelease.jsx
+++ src/Components/WarningRelease.jsx
@@ -1,10 +1,12 @@
+import "./WarningStyles.css"
+
 function WarningRelease({ deleteRelease, setReleaseWarning, idToDelete }) {
     return (
         <div className="warning-layout">
             <div className="warning-card">
                 <h2> Warning!</h2>
                 <p>Are you sure you want to delete this item?</p>
-                <p> This action cannot be undone</p>
+                <small> This action cannot be undone</small>
                 {<button className="yes-btn" onClick={() => deleteRelease(idToDelete)}>Yes</button>}
                 <button className="no-btn" onClick={() => setReleaseWarning(false)}>No</button>
             </div>
