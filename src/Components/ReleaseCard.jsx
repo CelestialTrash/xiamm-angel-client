10c4c486a967e473d4503f4ff9ab20738f5fd16e
Esteban Escalante <escalanteaveleyra@gmail.com>
2024-08-27 11:27:32 +0200

    release refactoring done

--- src/Components/ReleaseCard.jsx
+++ src/Components/ReleaseCard.jsx
@@ -1,15 +1,77 @@
 //CSS
-import './ReleaseCard.css'
+import "./ReleaseCard.css";
+//COMPONENTS
+import EditReleaseForm from "./EditReleaseForm";
+import WarningRelease from "./WarningRelease";
 
-function ReleaseCard({title, producer, imageUrl}) {
-    return(
-        <div className="card-info">
-            <img src={imageUrl} alt={title} />
-            <h3>{title}</h3>
-            <small>Produced by</small>
-            <p>{producer}</p>
+function ReleaseCard({
+    user, 
+    _id,
+    title, 
+    producer, 
+    imageUrl, 
+    displayWarning, 
+    releaseWarning, 
+    handleCancelEdit, 
+    deleteRelease, 
+    setReleaseWarning, 
+    idToDelete,
+    editReleaseId,
+    handleDisplayEditForm, 
+    getReleases,
+    date 
+}) {
+  return (
+    <div className="card-info">
+      <img src={imageUrl} alt={title} />
+      <h3>{title}</h3>
+      <small>Produced by</small>
+      <p>{producer}</p>
+
+      {user ? (
+        <div className="card-btn-container">
+          <button
+            className="edit-btn"
+            onClick={() => handleDisplayEditForm(_id)}
+          >
+            Edit
+          </button>
+          {editReleaseId === _id && (
+            <EditReleaseForm
+              id={_id}
+              title={title}
+              producer={producer}
+              date={date}
+              imageUrl={imageUrl}
+              cancelEdit={handleCancelEdit}
+              getReleases={getReleases}
+            />
+          )}
+          <button className="delete-btn" onClick={() => displayWarning(_id)}>
+            Delete
+          </button>
+          {releaseWarning && idToDelete === _id && (
+            <WarningRelease
+              deleteRelease={deleteRelease}
+              setReleaseWarning={setReleaseWarning}
+              idToDelete={_id}
+            />
+          )}
         </div>
-    )
+      ) : (
+        <div></div>
+      )}
+
+      <div className="corners">
+        <div className="top">
+          <div className="top-left"></div>
+        </div>
+        <div className="bottom">
+          <div className="bottom-right"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ReleaseCard
+export default ReleaseCard;
--- src/Pages/ReleasesPage.jsx
+++ src/Pages/ReleasesPage.jsx
@@ -1,134 +1,131 @@
 //COMPONENTS
-import ReleaseCard from "../Components/ReleaseCard"
-import CreateReleaseForm from "../Components/CreateReleaseForm"
-import EditReleaseForm from "../Components/EditReleaseForm"
-import WarningRelease from "../Components/WarningRelease"
-import Loader from "../Components/Loader"
+import ReleaseCard from "../Components/ReleaseCard";
+import CreateReleaseForm from "../Components/CreateReleaseForm";
+import Loader from "../Components/Loader";
 //CSS
-import "../Components/ReleaseCard.css"
-import "./ReleasesPage.css"
+import "../Components/ReleaseCard.css";
+import "./ReleasesPage.css";
 //REACT
-import { useState, useEffect,useContext } from "react"
-import { useNavigate } from "react-router-dom"
-import { AuthContext } from "../context/user.context"
+import { useState, useEffect, useContext } from "react";
+import { AuthContext } from "../context/user.context";
 //AXIOS
-import axios from "axios"
+import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL
-
+const API_URL = import.meta.env.VITE_API_URL;
 
 function ReleasesPage() {
-    //Create Form
-    const [displayForm, setDisplayForm] = useState(false)
-    const [releases, setReleases] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
-    
-    const handleDisplayCreateForm = () => {
-        return setDisplayForm(!displayForm)
-    }
+  const [displayForm, setDisplayForm] = useState(false);
+  const [releases, setReleases] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [editReleaseId, setEditReleaseId] = useState(null);
+  const [releaseWarning, setReleaseWarning] = useState(false);
+  const [idToDelete, setIdToDelete] = useState("");
+  const authToken = localStorage.getItem("Authorization");
 
-    const handleCancelCreate = () => {
-        setDisplayForm(false);
-    };
+  const handleDisplayCreateForm = () => {
+    return setDisplayForm(!displayForm);
+  };
 
-    //Edit Form
-    const [editReleaseId, setEditReleaseId] = useState(null)
-    
-    const handleDisplayEditForm = (releaseId) => {
-        return setEditReleaseId(releaseId)
-    }
+  const handleCancelCreate = () => {
+    setDisplayForm(false);
+  };
 
-    const handleCancelEdit = () => {
-        setEditReleaseId(null);
-    };
+  const handleDisplayEditForm = (releaseId) => {
+    return setEditReleaseId(releaseId);
+  };
 
-    //Get items
-    function getReleases() {
-        setIsLoading(true)
-        axios.get(`${API_URL}/api/releases`)
-            .then((response) => {
-                setReleases(response.data);
-                setIsLoading(false)
-            })
-            .catch((error) => {
-                console.error(error?.response.data.message);
-            });
-    }
+  const handleCancelEdit = () => {
+    setEditReleaseId(null);
+  };
 
-    useEffect(() =>{
-        getReleases()
-    }, [])
+  function getReleases() {
+    setIsLoading(true);
+    axios
+      .get(`${API_URL}/api/releases`)
+      .then((response) => {
+        setReleases(response.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error(error?.response.data.message);
+      });
+  }
 
+  useEffect(() => {
+    getReleases();
+  }, []);
 
-    //Delete item
-    const [releaseWarning, setReleaseWarning] = useState(false)
-    const [idToDelete, setIdToDelete] = useState("")
-    const authToken = localStorage.getItem("Authorization")
+  function deleteRelease(id) {
+    axios
+      .delete(`${API_URL}/api/releases/${id}`, {
+        headers: { Authorization: `Bearer ${authToken}` },
+      })
+      .then(() => getReleases())
+      .catch((error) => {
+        console.error(error?.response.data.message);
+      });
+  }
 
-    function deleteRelease(id) {
-        axios
-            .delete(`${API_URL}/api/releases/${id}`, { headers: { Authorization: `Bearer ${authToken}`} })
-            .then(() => getReleases())
-            .catch((error) => {
-                console.error(error?.response.data.message);
-            });
-    }
+  const displayWarning = (id) => {
+    setReleaseWarning(true);
+    setIdToDelete(id);
+  };
 
-    const displayWarning = (id) => {
-        setReleaseWarning(true);
-        setIdToDelete(id)
-    }
+  //User checker
+  const { user } = useContext(AuthContext);
 
-
-    //User checker
-    const {user} = useContext(AuthContext)
-    
-
-    return (
-        <>
-            {isLoading ? <Loader /> : (
-                <section id="releases-page">
-                    {user ? (
-                        <div id="release-form-container">
-                            <button className="add-btn" onClick={handleDisplayCreateForm}>Make Release</button>
-                            {displayForm && <CreateReleaseForm onClick={handleDisplayCreateForm} getReleases={getReleases} cancelCreate={handleCancelCreate} />}
-                        </div>
-                    ) : <div></div>
-                    }
-                    <ul id="card-container">
-                        {
-                            releases &&
-                            releases.map(({ _id, title, producer, imageUrl, date }) => {
-                                return (
-                                    <li className="card" key={_id}>
-                                        <ReleaseCard title={title} producer={producer} imageUrl={imageUrl} />
-
-                                        {user ? (
-                                            <div className="card-btn-container">
-                                                <button className="edit-btn" onClick={() => handleDisplayEditForm(_id)}>Edit</button>
-                                                {editReleaseId === _id && <EditReleaseForm id={_id} title={title} producer={producer} date={date} imageUrl={imageUrl} cancelEdit={handleCancelEdit} getReleases={getReleases} />}
-                                                <button className="delete-btn" onClick={() => displayWarning(_id)}>Delete</button>
-                                                {releaseWarning && idToDelete === _id && <WarningRelease deleteRelease={deleteRelease} setReleaseWarning={setReleaseWarning} idToDelete={_id} />}
-                                            </div>
-                                        ) : <div></div>}
-                                        <div className="corners">
-                                            <div className="top">
-                                                <div className="top-left"></div>
-                                            </div>
-                                            <div className="bottom">
-                                                <div className="bottom-right"></div>
-                                            </div>
-                                        </div>
-
-                                    </li>
-                                )
-                            })
-                        }
-                    </ul>
-                </section>
-            )}
-        </>
-    )
+  return (
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <section id="releases-page">
+          {user ? (
+            <div id="release-form-container">
+              <button className="add-btn" onClick={handleDisplayCreateForm}>
+                Make Release
+              </button>
+              {displayForm && (
+                <CreateReleaseForm
+                  onClick={handleDisplayCreateForm}
+                  getReleases={getReleases}
+                  cancelCreate={handleCancelCreate}
+                />
+              )}
+            </div>
+          ) : (
+            <div></div>
+          )}
+          <ul id="card-container">
+            {releases &&
+              releases.map(({ _id, title, producer, imageUrl, date }) => {
+                return (
+                  <li className="card" key={_id}>
+                    <ReleaseCard
+                      user={user}
+                      _id={_id}
+                      title={title}
+                      producer={producer}
+                      imageUrl={imageUrl}
+                      date = {date}
+                      handleDisplayEditForm={handleDisplayEditForm}
+                      displayWarning={displayWarning}
+                      handleCancelEdit={handleCancelEdit}
+                      editReleaseId={editReleaseId}
+                      deleteRelease={deleteRelease}
+                      releaseWarning={releaseWarning}
+                      setReleaseWarning={setReleaseWarning}
+                      idToDelete={idToDelete}
+                      getReleases = {getReleases}
+                    />
+                  </li>
+                );
+              })}
+          </ul>
+        </section>
+      )}
+    </>
+  );
 }
 
-export default ReleasesPage
+export default ReleasesPage;
