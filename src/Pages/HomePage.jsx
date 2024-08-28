39e6a3f168f98f40ab4df3037ecb820e84246481
Aquiles <Aquileshr92@gmail.com>
2024-08-28 16:02:48 +0200

    done

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,63 +1,68 @@
 //REACT
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 //CSS
-import './HomePage.css'
+import "./HomePage.css";
 
 //Axios
-import axios from 'axios';
+import axios from "axios";
 
 //ASSETS
 
-import soundcloudLogo from '../assets/SocialMediaSVG/soundcloud-svgrepo-com.svg';
-import spotifyLogo from '../assets/SocialMediaSVG/spotify-logo-fill-svgrepo-com.svg';
-import instagramLogo from '../assets/SocialMediaSVG/instagram-svgrepo-com.svg';
-import youtubeLogo from '../assets/SocialMediaSVG/youtube-168-svgrepo-com.svg';
-
+import soundcloudLogo from "../assets/SocialMediaSVG/soundcloud-svgrepo-com.svg";
+import spotifyLogo from "../assets/SocialMediaSVG/spotify-logo-fill-svgrepo-com.svg";
+import instagramLogo from "../assets/SocialMediaSVG/instagram-svgrepo-com.svg";
+import youtubeLogo from "../assets/SocialMediaSVG/youtube-168-svgrepo-com.svg";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
 function HomePage() {
-    const [latestRelease, setLatestRelease] = useState(null);
-    useEffect(() => {
-        
-        axios
-        .get(`${API_URL}/api/homepage/release`)
-            .then((response) => {
-                if (response.data && response.data.length > 0) {
-                    setLatestRelease(response.data[0]); 
-                }
-            })
-            .catch((error) => console.error("Error fetching latest release:", error));
-    }, []);
+  const [latestRelease, setLatestRelease] = useState(null);
+  useEffect(() => {
+    axios
+      .get(`${API_URL}/api/homepage/release`)
+      .then((response) => {
+        if (response.data && response.data.length > 0) {
+          setLatestRelease(response.data[0]);
+        }
+      })
+      .catch((error) => console.error("Error fetching latest release:", error));
+  }, []);
 
-    return(
-        <section className='home-page-section'>
-        <div className="social-links-section">
-            <div className='social-links-container'>
-            <img src={soundcloudLogo} className='soundcloud-logo' alt="SoundCloud Logo" />
-      <img src={spotifyLogo} className='spotify-logo' alt="Spotify Logo" />
-      <img src={instagramLogo} className='instagram-logo' alt="Instagram Logo" />
-      <img src={youtubeLogo} className='youtube-logo' alt="YouTube Logo" />
+  return (
+    <section className="home-page-section">
+      <div className="social-links-section">
+        <div className="social-links-container">
+          <img
+            src={soundcloudLogo}
+            className="soundcloud-logo"
+            alt="SoundCloud Logo"
+          />
+          <img src={spotifyLogo} className="spotify-logo" alt="Spotify Logo" />
+          <img
+            src={instagramLogo}
+            className="instagram-logo"
+            alt="Instagram Logo"
+          />
+          <img src={youtubeLogo} className="youtube-logo" alt="YouTube Logo" />
+        </div>
+      </div>
+      <div className="image-container">
+        {latestRelease && (
+          <div className="latest-release">
+            <Link target="_blank" to={latestRelease.linkToPlatform}>
+              {" "}
+              <img src={latestRelease.imageUrl} alt={latestRelease.title} />
+            </Link>
+            <h2>{latestRelease.title}</h2>
             
-                
-            </div>
-
-        </div>
-        <div className="image-container">
-                {latestRelease && (
-                    <div className="latest-release">
-                       <Link target="_blank" to={latestRelease.linkToPlatform}> <img src={latestRelease.imageUrl} alt={latestRelease.title} /></Link> 
-                    
-                    </div>
-                )}
-            </div>
-        <div className='title-container'>
+          </div>
+        )}
+      </div>
+      <div className="title-container">
         <h1>Ditto Artist</h1>
-        </div>
-        
-        </section>
-    )
-    
+      </div>
+    </section>
+  );
 }
 export default HomePage;
